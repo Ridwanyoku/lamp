c14b092501ae968338a2d961b0429e301178a6e4
Ridwanyoku <143164118+Ridwanyoku@users.noreply.github.com>
2024-05-11 20:37:46 +0700

    Update saklar.js

--- saklar.js
+++ saklar.js
@@ -10,29 +10,29 @@ function saklar() {
     let lampu3 = document.getElementById("lampu3");
 
     if (toggle1.checked) {
-      lampu1.src = "assets/images/on.png"
+      lampu1.src = "on.png"
     } 
     else {
-      lampu1.src = "assets/images/off.png";
+      lampu1.src = "off.png";
     }
     
     if (toggle2.checked) {
-      lampu2.src = "assets/images/on.png"
+      lampu2.src = "on.png"
     } else {
-      lampu2.src = "assets/images/off.png";
+      lampu2.src = "off.png";
     }
   
     if (toggle3.checked) {
-      lampu3.src = "assets/images/on.png"
+      lampu3.src = "on.png"
     } else {
-      lampu3.src = "assets/images/off.png";
+      lampu3.src = "off.png";
     }
 
     let toggleruang1 = document.getElementById("default-toggle-ruang1");
     if (toggleruang1.checked) {
-      lampu1.src = "assets/images/on.png";
-      lampu2.src = "assets/images/on.png";
-      lampu3.src = "assets/images/on.png";
+      lampu1.src = "on.png";
+      lampu2.src = "on.png";
+      lampu3.src = "on.png";
 
     } 
    
